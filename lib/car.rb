d69a43a3bd0bf5260c984f6977377c978c118080
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-01 15:42:12 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,19 +2,8 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
 
-  # attr_reader :wheel_size, :wheel_number
-
-  # def initialize(wheel_size, number)
-  #   @wheel_size = wheel_size
-  #   @number = number
-  # end
-
   def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
-
-  # def fill_up_tank
-  #   "filling up!"
-  # end
 
 end
